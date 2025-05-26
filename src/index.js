5b0bf5f06ec8d54a4cb5b6924cb73f4f5d6bae51
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-05-26 22:14:06 +0530

    Add react-router-dom dependency and implement routing in the application; enhance Footer and OpenAccount components with detailed content and structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{ BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Homepage from './landing _page/home/HomePage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
      <Homepage/>
   </React.StrictMode>
+  </BrowserRouter>
 );
