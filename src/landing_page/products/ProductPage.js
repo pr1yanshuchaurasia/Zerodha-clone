9c23eb36a03161e183abeb6db39f901f991348f6
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-05-28 10:32:48 +0530

    Refactor Hero, LeftSection, and ProductPage components for improved structure and content; enhance LeftSection with dynamic props for product details.

--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import Hero from './Hero';
-import LeftSection from './LeftSection';
-import RightSection from './RightSection';
-import Universe from './Universe';
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
 
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 function PricingPage() {
-    return ( 
-        <>
-    
-        <Hero/>
-        <LeftSection/>
-        <RightSection/>
-        <Universe/>
- 
-        </>
-     );
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURL="media/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURL="media/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <Universe />
+    </>
+  );
 }
 
 export default PricingPage;
