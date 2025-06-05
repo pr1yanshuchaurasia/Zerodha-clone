a8ba1bbbb63ed7d61acb5dba1478111c69719bcd
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-06-05 10:56:27 +0530

    feat: update WatchList component to display dynamic stock names and counts

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React,{useState} from "react";
+
+import {Tooltip, Grow} from "@mui/material";
+import {watchlist} from "../data/data";
 
 const WatchList = () => {
   return (
@@ -11,10 +14,13 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <p>{stock.name}</p>        })}
+      </ul>
     </div>
   );
 };
