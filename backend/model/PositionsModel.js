238a57ca1967dd871de010667ad60341aafd2a94
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-06-17 11:46:16 +0530

    fix: correct model name from 'order' to 'position' in PositionsModel.js

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -2,6 +2,6 @@ const {model} = require ("mongoose");
 
 const {PositionsSchema} = require("../schemas/PositionsSchema");
 
-const PositionsModel = new model ("order", PositionsSchema);
+const PositionsModel = new model ("position", PositionsSchema);
 
 module.exports ={PositionsModel};
